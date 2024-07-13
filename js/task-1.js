82c16fd1343668723bb34c625e7caf5f7b7314c0
Yuliia1991 <155849406+Yuliia1991@users.noreply.github.com>
2024-07-13 21:14:39 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -58,14 +58,73 @@
   //for (const values of keys);
   //console.log (values);
   //console.log (apartament["values"]);
-  function calculateTotalPrice(productName) {
-    const products = [
-      { name: "Radar", price: 1300, quantity: 4 },
-      { name: "Scanner", price: 2700, quantity: 3 },
-      { name: "Droid", price: 400, quantity: 7 },
-      { name: "Grip", price: 1200, quantity: 9 },
-    ];
-    let totalPrice = price * quantity;
-    for()
-  }
-  
+ //function multiply(first, second, ...args) {
+  //console.log(first, second, args);
+ //}
+ //multiply(1, 2); // 1 2
+//multiply(1, 2, 3); // 1 2 [3] 
+//multiply(1, 2, 3, 4); // 1 2 [3, 4]
+
+
+//REST
+//function addOverNum(value, ...args) {
+ // let total = 0;
+  //for (let i=0; i < args.length; i++){
+    //if(args[i] >= value) {
+      //total+=args[i];
+    //}
+  //}return total;
+//}
+//addOverNum(50, 15, 27);
+//addOverNum(10, 12, 4, 11, 48, 10, 8);
+
+//SPREAD
+//const temps = [14, -4, 25, 8, 11];
+//nsole.log(temps);
+
+//function getExtremeScores(scores) {
+ //return { best: Math.max(...scores),
+   //worst: Math.min(...scores) }; }
+
+//console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26])); // { best: 93, worst: 17 }
+ //console.log(getExtremeScores([19, 7, 4, 17, 81, 24])); // { best: 81, worst: 4 }
+
+ //function greet(name) {
+  //return `Welcome ${name}!`;
+//}
+
+// Викликаємо функцію greet і виводимо результат у консоль
+//console.log(greet("Mango")); // "Welcome Mango!"
+
+// Виводимо функцію greet у консоль, не викликаючи її
+//console.log(greet); // ƒ greet() {return `Welcome ${name}!`} 
+
+//function greet(name) {
+  //console.log(`Welcome ${name}!`);
+//}
+
+//function notify(name) {
+  //console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+//}
+
+//function registerGuest(name, callback) {
+  //console.log(`Registering ${name}!`);
+  //callback(name);
+//}
+//registerGuest("Mango", greet);
+//registerGuest("Mango", notify);
+
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+function makeMessage(pizzaName,callback) {
+return callback(pizzaName)
+}
+makeMessage("Royal Grand", makePizza);
+makeMessage("Ultracheese", deliverPizza);
